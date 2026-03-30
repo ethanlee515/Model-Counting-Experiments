d22852f00dc2645e0054d2d216b24be13fe1ae5d
Yi Lee <ylee1228@umd.edu>
2026-03-30 19:37:03 -0400

    Add toy example and devcontainer build

--- Rtl/src/Hello.scala
+++ Rtl/src/Hello.scala
@@ -1,3 +1,17 @@
-object Hello extends App {
-	println("hello world")
+import spinal.core._
+import spinal.lib._
+
+class ToyExample(n: Int) extends Component {
+  val physical_errors = in port Vec.fill(n, n)(Bool())
+  val transversals = Vec.fill(n)(Bool())
+  for(i <- 0 until n) {
+    transversals(i) := physical_errors(i).reduceBalancedTree(_ ^ _)
+  }
+  val s = transversals.sCount(True)
+  val logical_error = out port Bool()
+  logical_error := (s >= (n / 2) + 1)
+}
+
+object ToyVerilog extends App {
+  SpinalVerilog(new ToyExample(3))
 }
